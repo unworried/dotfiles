a8446754d30319dc540957f48e65e2824fe5f69a
unworried <unworried@tuta.io>
2026-05-09 19:28:19 -0700

    add: inital neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,12 @@
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
