a363410e464bfff5c703ccf3f41104b690c35684
unworried <unworried@tuta.io>
2026-05-13 17:24:25 -0700

    update: nvim config with a few nicities

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,8 +5,35 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 40 })
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    desc = "Cleanup whitespace before saving current buffer",
+    group = vim.api.nvim_create_augroup("whitespace_cleanup", {}),
+    pattern = '*',
+    command = [[%s/\s\+$//e]],
+})
+
+-- set
+local opt = vim.opt
+
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+opt.nu = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.smartindent = true
+
+opt.wrap = false
+
+opt.updatetime = 50
